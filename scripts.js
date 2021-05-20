c4d7bf3cb80a1d34f788d43440641f378c5566ac
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2021-05-20 09:38:59 -0700

    cleaned up code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Canvas Setup
-const canvas = document.querySelector("#canvas1");
+const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 let wX, wY;
 let hue = 0;
@@ -21,64 +21,31 @@ window.addEventListener("resize", function () {
   setCanvas();
 });
 
-// Get/Set Mouse Coords
-// const mouse = {
-//   x: 0,
-//   y: 0
-// };
+// Util Function to get a random vakue from a range
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-// const timesPerSecond = 50;
-// let wait = false;
-// canvas.addEventListener("mousemove", (e) => {
-  
-//   if (!wait) {
-//     mouse.x = e.clientX;
-//     mouse.y = e.clientY;
-//     for (let i = 0; i < 1; i++) {
-//       mouseBubbles.push(new Particle(mouse.x, mouse.y));
-//       wait = true;
-//       setTimeout(function () {
-//         wait = false;
-//       }, 300 / timesPerSecond);
-//     }
-//   }
-// });
-// canvas.addEventListener("mousemove", (e) => {
-//   mouse.x = e.clientX;
-//   mouse.y = e.clientY;
-//   for (let i = 0; i < 1; i++) {
-//     mouseBubbles.push(new Particle(mouse.x, mouse.y));
-//     wait = true;
-//     setTimeout(function () {
-//       wait = false;
-//     }, 300 / timesPerSecond);
-//   }
-// });
-
+// create a new bubble that animates up from the bottom of the screen at an interval
 const bubbleInterval = 2000;
 function bubbleBot() {
   if(document.hidden) {
     return
   }
   this.x = randomIntFromRange(0, wX);
-  // this.x = wX/2;
-  particleArray.push(new Particle(this.x, wY + 10));
+  bubbleArray.push(new Bubble(this.x, wY + 10));
 }
 setInterval(bubbleBot, bubbleInterval);
 
-// Random Util Function
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// Define bubble array
+let bubbleArray = [];
 
-// Define Particles
-let particleArray = [];
-let mouseBubbles = [];
-
+// define image to use in each bubble
 const sprite = new Image();
 sprite.src = 'blob.svg';
 
-function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY, axisX, axisY, size ) {
+// rotate the bubb
+function rotateAndPaintImage ( context, image, angleInRad, positionX, positionY, axisX, axisY, size ) {
   context.translate( positionX, positionY );
   context.rotate( angleInRad );
   context.drawImage( image, -axisX, -axisY, size, size );
@@ -86,12 +53,12 @@ function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY
   context.translate( -positionX, -positionY );
 }
 
-class Particle {
+// define bubble class
+class Bubble {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.r = randomIntFromRange(10, 20);
-    // this.speedX = randomIntFromRange(-.5, .5);
     this.speedX1 = 0;
     this.speedXVar = randomIntFromRange(-2, 2);
     this.speedX2 = this.speedXVar >= 0 ? 0.05 : -0.05;
@@ -100,22 +67,10 @@ class Particle {
     this.rotationSpeed = this.speedX1 < 0 ? -0.02 : 0.02;
     this.opacity = 1;
   }
-
   draw() {
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-    // ctx.fillStyle = this.color;
-    // ctx.lineWidth = 2;
-    // ctx.fill(p1);
-    // ctx.closePath();
-    // ctx.drawImage(sprite, this.x, this.y, this.r, this.r);
     ctx.globalAlpha = this.opacity;
     rotateAndPaintImage(ctx, sprite, this.rotation, this.x, this.y, this.r, this.r, this.r)
-    // if (this.y < 100) {
-    //   this.opacity -= .05;
-    // }
   }
-
   update() {
     this.x += this.speedX1;
     this.speedX1 += this.speedX2;
@@ -125,60 +80,45 @@ class Particle {
     this.y += this.speedY;
     this.rotation += this.rotationSpeed;
   }
-
   fade() {
     this.opacity -= 0.025;
   }
 }
 
-// Loop through particles to animate
-function handleParticles() {
-  for (let i = 0; i < particleArray.length; i++) {
-    particleArray[i].update();
-    particleArray[i].draw();
-
-    if (particleArray[i].y < 0 - this.r || particleArray[i].opacity <= 0.1) {
-      particleArray.splice(i, 1);
+// Loop through bubbles to animate
+function handleBubbles() {
+  for (let i = 0; i < bubbleArray.length; i++) {
+    bubbleArray[i].update();
+    bubbleArray[i].draw();
+    // if the bubble is above the top of the screen or if it is invisible, remove it from the array
+    if (bubbleArray[i].y < 0 - this.r || bubbleArray[i].opacity <= 0.1) {
+      bubbleArray.splice(i, 1);
       i--;
     }
   }
 }
 
-function handleMouseBubbles() {
-  for (let i = 0; i < mouseBubbles.length; i++) {
-    mouseBubbles[i].update();
-    mouseBubbles[i].fade();
-    mouseBubbles[i].draw();
-
-    if (mouseBubbles[i].y < 0 - this.r || mouseBubbles[i].opacity <= 0.1) {
-      mouseBubbles.splice(i, 1);
-      i--;
-    }
-  }
-}
-
-// Animate Particles
+// Animate Bubbles
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  handleParticles();
-  handleMouseBubbles();
+  handleBubbles();
   requestAnimationFrame(animate);
 }
-
-function release(){
-  for(let z = 0; z < 10; z++) {
-    bubbleBot();
-  }
-}
-// release();
 animate();
+
+// stop animation when browser tab is inactive
+const handleChange = (e) => {
+  document.hidden ? bubbleArray = [] : null;  
+}
+window.addEventListener('visibilitychange', handleChange)
 
 // Intersection Observer Setup
 const obObjects = document.querySelectorAll(".observed");
-const canvWrap = document.querySelector('.canvas-wrap');
-const canvWrap2 = document.querySelector('.canvas-wrap-2');
+const canvWrap = document.querySelector('.bg-wrap');
+const canvWrap2 = document.querySelector('.canvas-wrap');
 const octopus = document.querySelector('.octopus');
 const fish = document.querySelector('.fish');
+const gradient = document.querySelector('.seaweed-gradient');
 
 const offsetTop = `-10%`;
 const offsetBot = `-10%`;
@@ -204,6 +144,7 @@ function observerCallback(entries) {
       }
       if (el.dataset.color==='blue-03') {
         octopus.classList.add('in-view');
+        gradient.classList.add('in-view');
       }
     } else if (el.classList.contains("scroll-text") && entry.isIntersecting===false && entry.boundingClientRect.y>0) {
       document.body.classList.remove(color);
@@ -215,8 +156,9 @@ function observerCallback(entries) {
       if (el.dataset.color==='blue-02') {
         fish.classList.remove('in-view');
       }
-      if (el.classList.contains("octo-wrapper")) {
+      if (el.dataset.color==='blue-03') {
         octopus.classList.remove('in-view');
+        gradient.classList.remove('in-view')
       }
     } 
   });
@@ -228,15 +170,8 @@ obObjects.forEach((obj) => {
   observer.observe(obj);
 });
 
-// visibility test
-const handleChange = (e) => {
-  document.title = document.hidden ? "Come back!" : "Bubbles!";
-  document.hidden ? particleArray = [] : null;  
-}
-window.addEventListener('visibilitychange', handleChange)
-
+// split text of the highlighted text
 const textWrappers = document.querySelectorAll('.scroll-text .highlight');
 textWrappers.forEach(wrapper => {
   wrapper.innerHTML = wrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 })
-
